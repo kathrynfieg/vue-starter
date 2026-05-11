7b9195cc2d0d58129c30a53e316c8114b094f967
Kathryn Fieg <kathrynfieg@gmail.com>
2026-05-11 18:33:30 +0930

    feat: init types for shape element

---
+++ src/types/shape.ts
@@ -0,0 +1,42 @@
+export type ShapeType = 'rectangle' | 'circle'
+
+export type CanvasElementType = 'shape'
+
+export interface BaseElement {
+  id: string
+  type: CanvasElementType
+  x: number
+  y: number
+  rotation: number
+  zIndex: number
+  isLocked?: boolean
+}
+
+export interface ShapeStyle {
+  fill: string
+}
+
+interface ShapeBase extends BaseElement {
+  type: 'shape'
+  shapeType: ShapeType
+  style: ShapeStyle
+}
+
+export interface RectangleShape extends ShapeBase {
+  shapeType: 'rectangle'
+  width: number
+  height: number
+  cornerRadius?: number
+}
+
+export interface CircleShape extends ShapeBase {
+  shapeType: 'circle'
+  radius: number
+}
+
+export type ShapeElement = RectangleShape | CircleShape
+
+export interface CanvasBounds {
+  width: number
+  height: number
+}
